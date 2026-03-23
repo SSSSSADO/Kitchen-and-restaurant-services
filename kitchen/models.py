0f8254fd2e1b5642b1954c17cf0b4ce636ecafba
SSSSSADO <grushkovik.danil@gmail.com>
2026-03-23 15:43:26 +0200

    Created model Dish

--- kitchen/models.py
+++ kitchen/models.py
@@ -10,3 +10,21 @@ class DishType (models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Dish (models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    dish_type = models.ForeignKey(
+        DishType,
+        on_delete=models.CASCADE,
+        related_name="dishes"
+    )
+    cooks = models.ManyToManyField(
+        Cook,
+        related_name="dishes"
+    )
+
+    def __str__(self):
+        return self.name
