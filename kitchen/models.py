94b4bccd1bcc0a9835828cb96ec942121509a445
SSSSSADO <grushkovik.danil@gmail.com>
2026-03-23 22:55:10 +0200

    Add get_absolute_url method for DishType model

--- kitchen/models.py
+++ kitchen/models.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.db import models
 
 
-class Cook (AbstractUser):
+class Cook(AbstractUser):
     years_of_experience = models.IntegerField(null=True, blank=True)
 
     def get_absolute_url(self):
@@ -13,14 +13,17 @@ class Cook (AbstractUser):
         return self.username
 
 
-class DishType (models.Model):
+class DishType(models.Model):
     name = models.CharField(max_length=255)
+
+    def get_absolute_url(self):
+        return reverse("kitchen:dish-type-detail", kwargs={"pk": self.pk})
 
     def __str__(self):
         return self.name
 
 
-class Dish (models.Model):
+class Dish(models.Model):
     name = models.CharField(max_length=255)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
